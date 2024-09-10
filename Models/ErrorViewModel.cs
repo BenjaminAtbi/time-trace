7ae5531d2ccd1b01f2645a3f2b2dc2908d281f04
benjaminatbi <Benjaminatbi@gmail.com>
2024-09-09 20:39:10 -0700

    added project skeleton

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace time_trace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
