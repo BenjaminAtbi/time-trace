ae8e610253649fab0445d99f0d9114ab203a7794
benjaminatbi <Benjaminatbi@gmail.com>
2024-09-13 21:24:45 -0700

    added user subscription to schedules

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Claims;
 using time_trace.Data;
 using time_trace.Models;
 
@@ -11,10 +14,13 @@ namespace time_trace.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<ScheduleController> _logger;
-        public ScheduleController(ApplicationDbContext context, ILogger<ScheduleController> logger)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ScheduleController(ApplicationDbContext context, ILogger<ScheduleController> logger,
+            UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _logger = logger;
+            _userManager = userManager;
         }
 
         // GET: ScheduleController
@@ -27,11 +33,52 @@ namespace time_trace.Controllers
         public async Task<ActionResult> Edit(int? id)
         {
 
-            if (id == null) return RedirectToAction("Index");
+            if (id == null) return RedirectToAction(nameof(Index));
 
-            var schedule = await _context.Schedules.FirstOrDefaultAsync(s => s.Id == id);
-            
+            var schedule = await _context.Schedules
+                .Include(s => s.Users)
+                .Include(s => s.UserSchedules).ThenInclude(s => s.TimeSlots)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (schedule == null) return RedirectToAction(nameof(Index));
+
             return View(schedule);
+        }
+
+        // POST: ScheduleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, List<TimeSlot> TimeSlots)
+        {
+            _logger.LogInformation("\n######\nBeginning Edit Post route\n########");
+            if (id == null) return RedirectToAction(nameof(Index));
+
+            var activeUserName = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (activeUserName == null) throw new Exception("No User logged in");
+            _logger.LogInformation($"got activeuserid from claims: {activeUserName}");
+
+            var UserSchedule = await _context.UserSchedules
+                .Include(u => u.TimeSlots)
+                .FirstOrDefaultAsync(s => s.User.UserName == activeUserName && s.ScheduleId == id);
+            if (UserSchedule == null)
+            {
+                _logger.LogInformation($"failed to find UserSchedule with UserId: {activeUserName} and ScheduleID: {id}");
+
+                var schedule = await _context.Schedules.FirstOrDefaultAsync(s => s.Id == id);
+                var activeUser = await _context.ApplicationUsers.FirstOrDefaultAsync(u => u.Id == activeUserName);
+                _logger.LogInformation($"schedule: {schedule} activeUser: {activeUser}, activeusername: {activeUserName}");
+                if (schedule == null || activeUser == null) throw new Exception("Could not Retrieve schedule and Argument");
+
+                schedule.UserSchedules.Add(new UserSchedule
+                {
+                    Schedule = schedule,
+                    User = activeUser,
+                });
+            }
+            //UserSchedule.TimeSlots = TimeSlots;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Edit),id);
         }
 
         // GET: ScheduleController/Create
@@ -55,20 +102,7 @@ namespace time_trace.Controllers
             }
         }
 
-        // POST: ScheduleController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+
 
         // GET: ScheduleController/Delete/5
         public ActionResult Delete(int id)
@@ -89,6 +123,12 @@ namespace time_trace.Controllers
             {
                 return View();
             }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -61,7 +61,6 @@ builder.Services.ConfigureApplicationCookie(options =>
 {
     // Cookie settings
     options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
     options.LoginPath = "/Identity/Account/Login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
